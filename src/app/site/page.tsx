29fd25015f7b7fbbfe9074156e2523b15ef0d21e
Sonsawan Ngamsom <86834525+murphy6867@users.noreply.github.com>
2024-03-23 20:40:38 +0700

    [core] authentication

---
+++ src/app/site/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <section className="h-full w-full pt-36 relative flex items-center justify-center flex-col">
+        <div className="absolute bottom-0 left-0 right-0 top-0"></div>
+      </section>
+    </main>
+  );
+}
